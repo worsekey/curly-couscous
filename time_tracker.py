6383afac76c428f6657c9acd92fd73dffb41c7d0
worsekey <worsekey@proton.me>
2022-11-14 12:53:36 +0000

    successfully added the adding process with check whether cat exist or not

--- time_tracker.py
+++ time_tracker.py
@@ -18,8 +18,7 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Sends a message with category buttons if they exist,
         otherwise asks to add category"""
-    user = update.message.from_user
-    user_id = user['id']
+    user_id = update.message.from_user['id']
     cat_list = listcat(user_id)
     if cat_list:
         keyboard = [
@@ -36,11 +35,33 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await update.message.reply_text("Use /add <category> to add name of activity you would like to track.")
 
 
+async def add(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    """Checks if the command used right and adds category to the databse,
+       otherwise shows the message how to use it right.
+       Then asks user if that is what he/she/other wanted to add as their
+       category name and adds if yes, sends how to use /add command reminder again
+       if no"""
+    # TODO can I put this user_id and cat_name to the db_worker functions I wonder
+    cat_name = update.message.text.partition(' ')[2]
+    user_id = update.message.from_user['id']
+
+    if cat_name:
+        if setcat(cat_name, user_id):
+            await update.message.reply_text(f'{cat_name} is added to your category list.')
+        else:
+            await update.message.reply_text('Such a category already exist. Use /list to see all your categories.')
+
+    else:
+        await update.message.reply_text("Usage: /add <category>")
+
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
 
     #Handlers
     application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('add', add))
 
     application.run_polling()
