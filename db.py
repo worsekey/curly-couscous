35fe69a4aa822daadf4113979cf25c24cafa9449
worsekey <worsekey@proton.me>
2022-11-18 18:39:42 +0000

    initial commit; sqlalchemy orm instead of core

---
+++ db.py
@@ -0,0 +1,44 @@
+from sqlalchemy import (Date,
+                        Float,
+                        Column,
+                        Integer,
+                        String,
+                        ForeignKey,
+                        create_engine
+                        )
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer(), primary_key=True)
+    tuser_id = Column(String(12))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer(), primary_key=True)
+    cat_name = Column(String(50))
+    tuser_id = Column(String(), ForeignKey('user.tuser_id'))
+
+    time = relationship('Time', cascade='all, delete', backref='category', passive_deletes=True)
+
+
+class Time(Base):
+    __tablename__ = 'time_track'
+
+    id = Column(Integer(), primary_key=True)
+    cat_id = Column(String(), ForeignKey('category.id', ondelete='CASCADE'))
+    start_date = Column(Date())
+    start_time = Column(Float())
+    stop_date = Column(Date())
+    stop_time = Column(Float())
+    time_delta = Column(Float())
+
+
+engine = create_engine('sqlite:///db_2.sqlite')
+Base.metadata.create_all(engine)
